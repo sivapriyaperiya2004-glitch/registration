0ea383671b66e5d2e38c177c50fe27e6edfc0130
sivapriyaperiya2004-glitch <sivapriyaperiya2004@gmail.com>
2026-06-12 11:13:38 +0530

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,11 @@ import 'package:registration/Signupscreen.dart';
 import 'package:registration/forgotpassword.dart';
 import 'package:registration/login_screen.dart';
 import 'package:registration/welcome.dart';
-
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+void main() async{await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(
     MaterialApp(debugShowCheckedModeBanner: false, home: Welcome()),
   );
